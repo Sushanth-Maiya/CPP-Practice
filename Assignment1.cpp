f7ab38f31c8600a572bc823e56a094f204b94e10
Sushanth Maiya <fixed-term.sushanthganesh.maiya@in.bosch.com>
2025-05-26 13:05:46 +0530

    Updated Assignment code, added second answer.

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -16,45 +16,101 @@ using namespace std;
 // Takedata()                   Function to accept values for admno, sname, eng, science and invoke ctotal() to calculate total. 
 // Showdata()                   Function to display all the data members on the screen.   
 
-class Student
+// class Student
+// {
+//     int admno;
+//     char sname[20];
+//     float eng,math,science,total;
+//     float ctotal(void)
+//     {
+//         return(eng+math+science);
+//     }
+//     public:
+//     void Takedata(int a,const char* b,float e,float m,float s)
+//     {
+//         admno = a;
+//         strncpy(sname,b,sizeof(sname));
+//         sname[sizeof(sname)-1] = '\0';
+//         eng = e;
+//         math = m;
+//         science = s;
+//         total = ctotal();
+//     }
+//     void Showdata(void)
+//     {
+//         cout<<"Admission no. -> "<<admno<<endl;
+//         cout<<"Name -> "<<sname<<endl;
+//         cout<<"Marks: "<<endl;
+//         cout<<"English -> "<<eng<<endl<<"Maths -> "<<math<<endl<<"Science -> "<<science<<endl;
+//         cout<<"Total Marks-> "<<total<<endl;
+//     }
+// };
+// int main()
+// {
+//     Student s;
+//     int ad;
+//     float en,ma,sc,to;
+//     char na[20];
+//     cout<<"Enter the details: Name, admission number, marks(eng, maths, science)"<<endl;
+//     cin.getline(na,20);
+//     cin>>ad>>en>>ma>>sc;
+//     s.Takedata(ad,na,en,ma,sc);
+//     s.Showdata();
+//     return(0);
+// }
+
+// 2.  Define a class batsman with the following specifications: 
+// Private members: 
+// bcode                            4 digits code number 
+// bname                           20 characters 
+// innings, notout, runs        integer type 
+// batavg                           it is calculated according to the formula – 
+//                                      batavg =runs/(innings-notout) 
+// calcavg()                        Function to compute batavg 
+// Public members: 
+// readdata()                      Function to accept value from bcode, name, innings, notout and invoke the function                                       calcavg() 
+// displaydata()                   Function to display the data members on the screen. 
+
+class Batsman
 {
-    int admno;
-    char sname[20];
-    float eng,math,science,total;
-    float ctotal()
+    int bcode,innings,notout,runs;
+    char bname[20];
+    float batavg;
+    float calcavg(void)
     {
-        return(eng+math+science);
+        if(innings == notout)
+        return ((float)runs/innings);
+        else
+        return((float)runs/(innings - notout));
     }
     public:
-    void Takedata(int a,const char* b,float e,float m,float s)
+    void readdata(const char* name,int c,int i,int n,int r)
     {
-        admno = a;
-        strncpy(sname,b,sizeof(sname));
-        sname[sizeof(sname)-1] = '\0';
-        eng = e;
-        math = m;
-        science = s;
-        total = ctotal();
+        strncpy(bname,name,sizeof(bname));
+        bname[sizeof(bname)-1]= '\0';
+        bcode = c;
+        innings = i;
+        notout = n;
+        runs = r;
+        batavg = calcavg();
     }
-    void Showdata(void)
+    void displaydata(void)
     {
-        cout<<"Admission no. -> "<<admno<<endl;
-        cout<<"Name -> "<<sname<<endl;
-        cout<<"Marks: "<<endl;
-        cout<<"English -> "<<eng<<endl<<"Maths -> "<<math<<endl<<"Science -> "<<science<<endl;
-        cout<<"Total Marks-> "<<total<<endl;
+        cout<<"Batter name: "<<bname<<endl;
+        cout<<"Innings played: "<<innings<<" "<<"Runs: "<<runs<<endl;
+        cout<<"Times Notout: "<<notout<<" "<<"Batting Average: "<<batavg<<endl;
     }
 };
 int main()
 {
-    Student s;
-    int ad;
-    float en,ma,sc,to;
-    char na[20];
-    cout<<"Enter the details: Name, admission number, marks(eng, maths, science)"<<endl;
-    cin.getline(na,20);
-    cin>>ad>>en>>ma>>sc;
-    s.Takedata(ad,na,en,ma,sc);
-    s.Showdata();
-    return(0);
+    int co,in,ru,no;
+    char name[20];
+    cout<<"Enter the player name-> ";
+    cin.getline(name,20);
+    cout<<endl<<"Enter player details (Innings,Runs,Notout,Code)-> "<<endl;
+    cin>>in>>ru>>no>>co;
+    Batsman b;
+    b.readdata(name,co,in,no,ru);
+    b.displaydata();
+    return 0;
 }
