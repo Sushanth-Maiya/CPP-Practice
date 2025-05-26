efe777d9571f4b8ae543b34992206b731ee1d7a8
Sushanth Maiya <fixed-term.sushanthganesh.maiya@in.bosch.com>
2025-05-26 13:43:59 +0530

    Updated question 3

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -71,46 +71,101 @@ using namespace std;
 // readdata()                      Function to accept value from bcode, name, innings, notout and invoke the function                                       calcavg() 
 // displaydata()                   Function to display the data members on the screen. 
 
-class Batsman
+// class Batsman
+// {
+//     int bcode,innings,notout,runs;
+//     char bname[20];
+//     float batavg;
+//     float calcavg(void)
+//     {
+//         if(innings == notout)
+//         return ((float)runs/innings);
+//         else
+//         return((float)runs/(innings - notout));
+//     }
+//     public:
+//     void readdata(const char* name,int c,int i,int n,int r)
+//     {
+//         strncpy(bname,name,sizeof(bname));
+//         bname[sizeof(bname)-1]= '\0';
+//         bcode = c;
+//         innings = i;
+//         notout = n;
+//         runs = r;
+//         batavg = calcavg();
+//     }
+//     void displaydata(void)
+//     {
+//         cout<<"Batter name: "<<bname<<endl;
+//         cout<<"Innings played: "<<innings<<" "<<"Runs: "<<runs<<endl;
+//         cout<<"Times Notout: "<<notout<<" "<<"Batting Average: "<<batavg<<endl;
+//     }
+// };
+// int main()
+// {
+//     int co,in,ru,no;
+//     char name[20];
+//     cout<<"Enter the player name-> ";
+//     cin.getline(name,20);
+//     cout<<endl<<"Enter player details (Innings,Runs,Notout,Code)-> "<<endl;
+//     cin>>in>>ru>>no>>co;
+//     Batsman b;
+//     b.readdata(name,co,in,no,ru);
+//     b.displaydata();
+//     return 0;
+// }
+
+// 3. Define a class TEST in C++ with following description: 
+// Private Members 
+// TestCode of type integer 
+// Description of type string 
+// NoCandidate of type integer 
+// CenterReqd (number of centers required) of type integer 
+// A member function CALCNTR() to calculate and return the number of centers as 
+// (NoCandidates/100+1) 
+// Public Members 
+// -  A function SCHEDULE() to allow user to enter values for TestCode, Description, NoCandidate & call function CALCNTR() to calculate the number of Centres 
+// - A function DISPTEST() to allow user to view the content of all the data members  
+
+class test
 {
-    int bcode,innings,notout,runs;
-    char bname[20];
-    float batavg;
-    float calcavg(void)
+    int TestCode,NoCandidate,CenterReqd;
+    string Description;
+    int CALCNTR(void)
     {
-        if(innings == notout)
-        return ((float)runs/innings);
-        else
-        return((float)runs/(innings - notout));
+        return (NoCandidate/100+1);
     }
     public:
-    void readdata(const char* name,int c,int i,int n,int r)
+    void SCHEDULE(int t,int n,string d)
     {
-        strncpy(bname,name,sizeof(bname));
-        bname[sizeof(bname)-1]= '\0';
-        bcode = c;
-        innings = i;
-        notout = n;
-        runs = r;
-        batavg = calcavg();
+        TestCode = t;
+        NoCandidate = n;
+        Description = d;
+        CenterReqd = CALCNTR();
     }
-    void displaydata(void)
+    void DISPTEST(void)
     {
-        cout<<"Batter name: "<<bname<<endl;
-        cout<<"Innings played: "<<innings<<" "<<"Runs: "<<runs<<endl;
-        cout<<"Times Notout: "<<notout<<" "<<"Batting Average: "<<batavg<<endl;
+        cout<<"TEST Content: "<<endl;
+        cout<<"Test Code: "<<TestCode<<endl;
+        cout<<"Number of candidates: "<<NoCandidate<<endl;
+        cout<<"Description: "<<Description<<endl;
+        cout<<"Centers Required: "<<CenterReqd<<endl;
     }
 };
 int main()
 {
-    int co,in,ru,no;
-    char name[20];
-    cout<<"Enter the player name-> ";
-    cin.getline(name,20);
-    cout<<endl<<"Enter player details (Innings,Runs,Notout,Code)-> "<<endl;
-    cin>>in>>ru>>no>>co;
-    Batsman b;
-    b.readdata(name,co,in,no,ru);
-    b.displaydata();
+    int tc,nc;
+    string de;
+    cout<<"Enter details: "<<endl;
+    cout<<"Description-> ";
+    getline(cin,de);
+    cout<<endl<<"Test Code-> ";
+    cin>>tc;
+    cout<<endl<<"No. of candidates: ";
+    cin>>nc;
+    cout<<endl;
+    test t;
+    t.SCHEDULE(tc,nc,de);
+    t.DISPTEST();
     return 0;
 }
